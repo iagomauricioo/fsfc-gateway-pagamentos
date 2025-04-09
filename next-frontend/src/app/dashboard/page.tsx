6af6806250ac04bff887b1f940e640fcdbfbe12d
Iago Mauricio <iagomauricio7@gmail.com>
2025-04-09 13:00:51 -0300

    funcionando as telas e rotas do nextjs

--- next-frontend/src/app/dashboard/page.tsx
+++ next-frontend/src/app/dashboard/page.tsx
@@ -19,7 +19,7 @@ export default function DashboardPage() {
                 Gerencie suas faturas e acompanhe os pagamentos
               </CardDescription>
             </div>
-            <Link href="/dashboard/new-invoice">
+            <Link href="/dashboard/invoice/new">
               <Button className="bg-indigo-600 hover:bg-indigo-700">
                 <Plus className="mr-2 h-4 w-4" />
                 Nova Fatura
